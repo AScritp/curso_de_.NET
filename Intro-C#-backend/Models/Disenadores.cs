6732155d74adf6ece5f4b41edd7e029e563e01ba
AScritp <miguel.a.lopez.m0502@gmail.com>
2025-05-29 23:14:18 +0200

    Version_9:_Servicios_WEB_GET_POST_PUT_DELETE

---
+++ Intro-C#-backend/Models/Disenadores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Intro_C__backend.Models
+{
+    public class Disenadores
+    {
+        [JsonPropertyName("_id")] //<--Esto permite "Traducir" la propiedades del JSON  y del objeto para evitar conflicto
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastname")]
+        public string Lastname { get; set; }
+        
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        
+        [JsonPropertyName("picture")]
+        public string Picture { get; set; }
+
+    }
+}
--- Intro-C#-backend/Program.cs
+++ Intro-C#-backend/Program.cs
@@ -1,32 +1,62 @@
-﻿using System;
+﻿using Intro_C__backend.Models;
+using System;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text.Json; //--imports para la des/serialización JSON
 using System.Text.Json.Serialization;//--imports para la des/serialización JSON
-using Intro_C__backend.Models;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace Intro_C__backend{
     class Program
     {
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            /* Fase de serialización de objetos a JSON
-            var sofa = new Sofases("01", "", 0, "00007", "Sofa NODE.JS Style", "Azul");
-            //El método Serialize de la clase JsonSerializer convierte un objeto en una cadena JSON.
-            string myJson = JsonSerializer.Serialize(sofa);
-            //File es una clase estática que nos permite trabajar con ficheros de forma sencilla, aquí hacemos uso del método WriteAllText para escribir el JSON en un fichero.
-            File.WriteAllText("objeto.txt",myJson);
-             */
+            //Variable con la URL de mi API
+            string url = "http://localhost:3000/api/disenadores/";
+            //Creo Objeto a partir de la clase HttpClient
+            var client = new HttpClient();  
+            //Metodo constructor de objeto disenador de la Clase Disenadores
+            Disenadores disenador = new Disenadores
+            {
+                Name = "Sergi",
+                Lastname = "Romero",
+                Description = "Profesor",
+                Picture = "https://example.com/picture.jpg"
+            };
 
-            // Fase de deserialización de JSON a objetos
-            //El método ReadAllText de la clase File lee el contenido de un fichero y lo devuelve como una cadena de texto.
-            string filetJson = File.ReadAllText("objeto.txt");
-            //Llegados a este punto, en necesario habilitar un constructor sin parámetros en la clase Sofases para poder deserializar correctamente el objeto desde JSON.
-            Sofases sofa = JsonSerializer.Deserialize<Sofases>(filetJson);
+            string textjson = JsonSerializer.Serialize<Disenadores>(disenador);
+
+            //Creo un nuevo objeto que tomara los datos serializados de disenador, luego codificamos el text en UTF-8 y por ultimo le integramos una propieda que tienen los JSON
+            // Esto sirve para Insertar(POST) datos nuevos o cambiarlos (PUT)
+            HttpContent content = new StringContent(textjson, System.Text.Encoding.UTF8, "application/json");
+
+            //==== Las distitas consulta HTTP (Delete, Put(UPDATE), POST(CREATE))=====
+            var HttpResponse = await client.DeleteAsync("http://localhost:3000/api/disenadores/68389738b11487c6f8f7baf0");
+            //var HttpResponse = await client.PutAsync("http://localhost:3000/api/disenadores/68389738b11487c6f8f7baf0", content);
+            //var HttpResponse = await client.PostAsync(url, content);
+            //==== Las distitas consulta HTTP (Delete, Put(UPDATE), POST(CREATE))=====
+
+            //Condicional que si recibe un OK por parte del servidor entonces hace lo siguiente
+            if (HttpResponse.IsSuccessStatusCode)
+            {
+                //variable que usa el GET del HTTP
+                var HttpGet = await client.GetAsync(url);
+
+                //variable que toma el contenido de la variable "HttpGet" para luego leerlo
+                var read = await HttpGet.Content.ReadAsStringAsync();
+                //Deserializamos, hay que tener en cuenta que el JSON es una lista de objetos, por ende usamos el List y le ponemos la clase Disenadores para que las propiedades correspondan
+                List<Disenadores> desjsonList = JsonSerializer.Deserialize<List<Disenadores>>(read);
+
+                //Bucle que imprime una seria de varoles por cada linea que recorra del contenido
+                foreach (var item in desjsonList)
+                {
+                    Console.WriteLine($"ID:{item.Id} Name: {item.Name}, LastName: {item.Lastname}, Descripción: {item.Description}, Picture: {item.Picture} ");
+                }
+            }
         }
     }
-
-   
 }   
